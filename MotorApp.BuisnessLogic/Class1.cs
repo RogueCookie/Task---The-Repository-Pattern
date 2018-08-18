1d2d2680d6f65415bfaafad954a5accf29d8c266
Valeriia Vaganova <v9527906422@gmail.com>
2018-08-18 16:30:40 +0200

    Created MotorContext + webConfig

--- MotorApp.BuisnessLogic/Class1.cs
+++ MotorApp.BuisnessLogic/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MotorApp.DataAccess
+namespace MotorApp.BuisnessLogic
 {
     public class Class1
     {
---
+++ MotorApp.BuisnessLogic/MotorContext.cs
@@ -0,0 +1,20 @@
+﻿using MotorApp.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorApp.BuisnessLogic
+{
+    public class MotorContext : DbContext
+    {
+        public MotorContext() : base("name = MotorAppConnectionString")  //() entity fram desides where we want to create Db,what name to dive Db
+        {
+        }
+
+        public DbSet<Motor> Motors { get; set; }
+
+    }
+}
