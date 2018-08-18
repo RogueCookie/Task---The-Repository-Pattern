609c1f37de2ab4a7f499c70183b5b28ccc8644b1
Valeriia Vaganova <v9527906422@gmail.com>
2018-08-18 18:23:52 +0200

    Fillled UnitTest

---
+++ Motor.App.UnitTest/MotorRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MotorApp.BuisnessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorApp.UnitTest
+{
+    [TestClass]
+    public class MotorRepositoryTest
+    {
+        MotorRepository Repo;
+        [TestInitialize]
+        public void TestSetup()
+        {
+            MotorInitializeDB db = new MotorInitializeDB();
+            System.Data.Entity.Database.SetInitializer(db);
+            Repo = new MotorRepository();
+        }
+        
+        [TestMethod]
+        public void IsRepositoryInitializeWithValidNumberOdData()
+        {
+            var result = Repo.GetMotors();
+            Assert.IsNotNull(result);
+            var numberOfRecords = result.ToList().Count;
+            Assert.AreEqual(2, numberOfRecords);
+        }
+    }
+}
