5c99fc8188379d89b5c1c4b453435893f155cd12
Valeriia Vaganova <v9527906422@gmail.com>
2018-08-18 17:38:43 +0200

    Creating MotorRepository

---
+++ MotorApp.BuisnessLogic/MotorRepository.cs
@@ -0,0 +1,37 @@
+﻿using MotorApp.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorApp.BuisnessLogic
+{
+    public class MotorRepository : IMotorRepository
+    {
+        public void Add(Motor p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Motor p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Motor FindById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Motor> GetMotors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
