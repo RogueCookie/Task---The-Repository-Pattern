4e5096ede35b643b0a7429c424d669575010079b
Valeriia Vaganova <v9527906422@gmail.com>
2018-08-18 15:19:20 +0200

    Add project files.

---
+++ MotorApp.DataAccess/Motor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorApp.DataAccess
+{
+    public class Motor
+    {
+        public int Id { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        [Required]
+        public string MotorName { get; set; }
+
+        public decimal Current { get; set; }
+
+        public decimal DifferenceC { get; set; }
+
+        public int Revs { get; set; }
+
+        public int DifferenceR { get; set; }
+
+        public int Pressure { get; set; }
+
+        public int DifferenceP { get; set; }
+    }
+}
