12d1c2d8a1a719b82869e1520bd9d018428bbc40
Valeriia Vaganova <v9527906422@gmail.com>
2018-08-18 15:42:26 +0200

    Added IMotorRepository

---
+++ MotorApp.DataAccess/IMotorRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorApp.DataAccess
+{
+    /// <summary> That's will be define which database can be performed on our Data Entity </summary>
+    public interface IMotorRepository
+    {
+        void Add(Motor p);
+        void Edit(Motor p);
+        void Remove(int Id);
+
+        IEnumerable<Motor> GetMotors();
+        Motor FindById(int Id);
+    }
+}
--- MotorApp.DataAccess/Motor.cs
+++ MotorApp.DataAccess/Motor.cs
@@ -9,22 +9,28 @@ namespace MotorApp.DataAccess
 {
     public class Motor
     {
+        [Required]
         public int Id { get; set; }
 
+        [Required]
         public DateTime TimeStamp { get; set; }
 
         [Required]
         [MaxLength(100)]
         public string MotorName { get; set; }
 
+        [Required]
         public decimal Current { get; set; }
 
+        
         public decimal DifferenceC { get; set; }
 
+        [Required]
         public int Revs { get; set; }
 
         public int DifferenceR { get; set; }
 
+        [Required]
         public int Pressure { get; set; }
 
         public int DifferenceP { get; set; }
